a9e6b266080f0d1ff0f22f8c1b1961efe113e111
15cf0408 <tom.lemarchandcholot@gmail.com>
2025-03-26 16:57:57 +0100

    listViewRight

--- src/main/java/info/prog/agario/view/GameView.java
+++ src/main/java/info/prog/agario/view/GameView.java
@@ -5,9 +5,13 @@ import javafx.animation.AnimationTimer;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.util.Duration;
 import info.prog.agario.model.world.MiniMap;
@@ -29,19 +33,38 @@ public class GameView implements EffectListener {
         root = new Pane();
         world = new GameWorld(pseudo);
         controller = new GameController(world, root);
-        controller.setEffectListener(this); // Définir GameView comme écouteur d'effet
         scene = new Scene(mainRoot, 800, 600);
         miniMap = new MiniMap(world.getEntities(), world.getPlayer());
-        controller.initialize();
-
-        // Initialisation de la ListView
         listView = new ListView<>();
+
+        controller.setEffectListener(this);
+        scene.getStylesheets().add(getClass().getResource("/info/prog/agario/view/style.css").toExternalForm());
+        controller.initialize();
         effectsList = FXCollections.observableArrayList();
         listView.setItems(effectsList);
+        listView.setVisible(false);
+
+
+        effectsList.addListener((ListChangeListener<String>) change -> {
+            listView.setVisible(!effectsList.isEmpty());
+            adjustListViewSize();
+        });
+
+        mainRoot.setOnMouseMoved(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                updateListeViewPlace();
+                System.out.println("***************************************************************");
+            }
+        });
+
 
         miniMap.updateEntities(world.getEntities(), world.getPlayer());
         mainRoot.getChildren().add(root);
         mainRoot.getChildren().add(miniMap);
+
+
+
         mainRoot.getChildren().add(listView);
 
         new AnimationTimer() {
@@ -52,12 +75,12 @@ public class GameView implements EffectListener {
         }.start();
     }
 
+    public void updateListeViewPlace(){
+        listView.setLayoutX(mainRoot.getWidth()-250);
+    }
     @Override
     public void onEffectActivated(String effect, int duration) {
-        // Ajouter l'effet à la liste
         effectsList.add(effect);
-
-        // Supprime l'effet après 'duration' secondes
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(duration), e -> effectsList.remove(effect))
         );
@@ -67,4 +90,13 @@ public class GameView implements EffectListener {
     public Scene getScene() {
         return scene;
     }
+
+
+    private void adjustListViewSize() {
+        int itemCount = effectsList.size();
+        double itemHeight = 30  ;
+        double listHeight = itemCount * itemHeight+30;
+        listView.setPrefHeight(listHeight);
+    }
 }
+
